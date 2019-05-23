8f6e4b53757f3e0aafe199d4deb52716c049ed56
Defne <defnesurujon@gmail.com>
2019-05-23 17:05:26 -0400

    update add_to_cluster, new test output

--- BF_clustering/add_to_clustering.m
+++ BF_clustering/add_to_clustering.m
@@ -1,4 +1,4 @@
-% DS Feb 2019
+% DS May 2019
 % This function takes in a new fasta file, and an existing clustering
 % partition (a .mat data file, output of runBF_all), and assigns the best
 % cluster to each of the incoming sequences. 
@@ -7,45 +7,61 @@
 % [input] clusterdatafile: .mat file containing clustering information for
 % sequenes in treeseqsfile. This should at least have the variables
 % 'trees', 'clusterres_ext' and 'consclust'
-% [output] new_consclust: final consensus clustering of new sequences. Cell
-% i has the output for method i (Ward/Kmeans/...etc)
-% [output] new_clusterres_ext: best cluster assignment for each tree (this
-% is used for consensus clustering to get to new_consclust). Cell
-% i has the output for method i (Ward/Kmeans/...etc)
+% [input] allmethods: whether all downstream clustering methods are used (if false, assume only a single method is used)
+% a .csv and .mat file will be written in the same directory as clusterdatafile
 
-function [new_consclust, new_clusterres_ext] = add_to_clustering(newseqsfile, treeseqsfile, clusterdatafile,ntree)
-
+function add_to_clustering(newseqsfile, treeseqsfile, clusterdatafile, allmethods)
 newseqs = fastaread(newseqsfile);
-% make a boundary tree out of newseqsfile
-eps = 0.1;
-max_deg = 10;
-[newtree,newtree_node_ref,newdata_order_ix] = boundary_tree(newseqs, eps, max_deg);
-% grab the sequences present in newtree
-newtreeseqs = newseqs(arrayfun(@(x) newtree{x}{1}, [1:length(newtree)]));
+[filepath,name,ext] = fileparts(newseqsfile);
+[clstfilepath,clstname,clstext] = fileparts(clusterdatafile);
+outfilename = strcat(name, '.mat');
+outfilename = fullfile(clstfilepath, outfilename);
+outcsvname = strcat(name,'.csv');
+outcsvname = fullfile(clstfilepath, outcsvname);
+
+if allmethods == true
+	[new_consclust, new_clusterres_ext] = add_to_clustering_all(newseqsfile, treeseqsfile, clusterdatafile)
+	
+	disp('Saving all data');
+	% save all data into a file
+	save(outfilename,'new_consclust',...
+		'new_clusterres_ext','-v7.3')
+
+	disp('Writing consensus clusters to csv');
+	mymat = strings(length(newseqs),8);
+	for method = 1:7
+		for i = 1:length(newseqs)
+			mymat(i,1) = newseqs(i).Header;
+			mymat(i,method+1) = string(new_consclust{method}(i));
+		end
+	end
+
+	mymatheaders = string({'Sequence','Ward','Kmeans',...
+	'Kmeans vectorized','Spectral NN',...
+	'Spectral SM','Spectral JW', 'MCL'});
+	mymat  = [mymatheaders;mymat];
+	cell2csv(outcsvname,mymat);
+
+else
+	[new_consclust, new_clusterres_ext] = add_to_clustering_single(newseqsfile, treeseqsfile, clusterdatafile)
+	
+	disp('Saving all data');
+	% save all data into a file
+	save(outfilename,'new_consclust',...
+		'new_clusterres_ext','-v7.3')
+
+	disp('Writing consensus clusters to csv');
+	mymat = strings(length(newseqs),2);
+
+	for i = 1:length(newseqs)
+		mymat(i,1) = newseqs(i).Header;
+		mymat(i,2) = string(new_consclust(i));
+	end
 
 
-treeseqs = fastaread(treeseqsfile);
-load(clusterdatafile, 'trees', 'clusterres_ext', 'consclust');
-
-new_consclust = cell(1,7);
-% assign to a representative for each method, each BT
-new_clusterres_ext = cell(1,7); 
-for method_ix = 1:7
-    ext_thismethod = zeros(length(newtreeseqs), ntree);
-   for tree_ix = 1:ntree 
-        [rep_seq, rep_seq_tree_ix] = find_closest_on_BF(newtreeseqs,trees{tree_ix},treeseqs);
-        clusters_thistree = clusterres_ext{method_ix}(rep_seq, tree_ix);
-        ext_thismethod(:, tree_ix)=clusters_thistree;
-        
-   end
-   new_clusterres_ext{method_ix} = ext_thismethod;
-   % find closest neighbor on consensus. 
-    Idx = knnsearch(clusterres_ext{method_ix}, new_clusterres_ext{method_ix});
-    % grab the consensus cluster assignment
-    cons_assn = consclust{method_ix}(Idx);
-    %extend consensus clustering
-    cons_assn_ext = extendBF_DS(cons_assn, newtree, newtree_node_ref);
-    new_consclust{method_ix} = cons_assn_ext;
+	mymatheaders = string({'Sequence','Cluster'});
+	mymat  = [mymatheaders;mymat];
+	cell2csv(outcsvname,mymat);
 end
 
 
---
+++ BF_clustering/add_to_clustering_single.m
@@ -0,0 +1,50 @@
+% DS Feb 2019
+% This function takes in a new fasta file, and an existing clustering - single downstream method
+% partition (a .mat data file, output of runBF_all), and assigns the best
+% cluster to each of the incoming sequences. 
+% [input] newseqsfile: fasta file of new sequences
+% [input] treeseqsfile: fasta file used for the existing clustering
+% [input] clusterdatafile: .mat file containing clustering information for
+% sequenes in treeseqsfile. This should at least have the variables
+% 'trees', 'clusterres_ext' and 'consclust'
+% [output] new_consclust: final consensus clustering of new sequences. 
+% [output] new_clusterres_ext: best cluster assignment for each tree (this
+% is used for consensus clustering to get to new_consclust). 
+
+function [new_consclust, new_clusterres_ext] = add_to_clustering_single(newseqsfile, treeseqsfile, clusterdatafile)
+
+newseqs = fastaread(newseqsfile);
+% make a boundary tree out of newseqsfile
+eps = 0.1;
+max_deg = 10;
+[newtree,newtree_node_ref,newdata_order_ix] = boundary_tree(newseqs, eps, max_deg);
+% grab the sequences present in newtree
+newtreeseqs = newseqs(arrayfun(@(x) newtree{x}{1}, [1:length(newtree)]));
+
+
+treeseqs = fastaread(treeseqsfile);
+load(clusterdatafile, 'trees', 'clusterres_ext', 'consclust');
+ntree = size(trees, 2);
+
+% assign to a representative for each BT
+
+new_clusterres_ext = zeros(length(newtreeseqs), ntree);
+for tree_ix = 1:ntree 
+	[rep_seq, rep_seq_tree_ix] = find_closest_on_BF(newtreeseqs,trees{tree_ix},treeseqs);
+	clusters_thistree = clusterres_ext(rep_seq, tree_ix);
+	new_clusterres_ext(:, tree_ix)=clusters_thistree;
+	
+end
+% find closest neighbor on consensus. 
+Idx = knnsearch(clusterres_ext, new_clusterres_ext);
+% grab the consensus cluster assignment
+cons_assn = consclust(Idx);
+%extend consensus clustering
+cons_assn_ext = extendBF_DS(cons_assn, newtree, newtree_node_ref);
+new_consclust = cons_assn_ext;
+
+
+
+
+
+end
